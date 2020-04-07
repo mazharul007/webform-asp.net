74e13070767388afda4d30f47f86e98a03c68069
unknown <mohammad.mazharul.hoque7@gmail.com>
2020-04-07 12:44:59 +0600

    file upload control added

---
+++ webformImportantTopics/webformImportantTopics/fileUploadCtrl.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="fileUploadCtrl.aspx.cs" Inherits="webformImportantTopics.fileUploadCtrl" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:FileUpload ID="fileUpload" runat="server" />
+            <br />
+            <asp:Button ID="btnSave" runat="server" Text="Save" OnClick="btnSave_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ webformImportantTopics/webformImportantTopics/fileUploadCtrl.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace webformImportantTopics
+{
+    public partial class fileUploadCtrl : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+
+            string ext = System.IO.Path.GetExtension(fileUpload.FileName);
+
+            if(ext == "pptx")
+            {
+                string path = Server.MapPath("~\\Files\\");
+
+                fileUpload.SaveAs(path + fileUpload.FileName);
+            }
+            else
+            {
+                Response.Write("file extension except pptx are not allowed");
+            }
+            
+        }
+    }
+}
